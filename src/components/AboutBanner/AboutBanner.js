880e59bede2bcaef5eb2a405bac6546a509f2c08
kritik <kritikverma2002@gmail.com>
2022-06-14 23:02:51 +0530

    added react-router-dom

---
+++ src/components/AboutBanner/AboutBanner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AboutBanner.scss";
+
+function AboutBanner() {
+  return (
+    <div className="aboutBanner">
+      <div className="aboutBanner__img">
+        <div>
+          <h1>About Us</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutBanner;
---
+++ src/pages/About/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutUs from "../../components/AboutUs/AboutUs";
+import AboutBanner from "../../components/AboutBanner/AboutBanner";
+import "./About.scss";
+
+function About() {
+  return (
+    <div>
+      <AboutBanner />
+      <AboutUs />
+      About
+    </div>
+  );
+}
+
+export default About;
