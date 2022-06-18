559d78e66e2fb11150bff5e70a66d877184fa546
yamuprajapati05@gmail.com <yamuprajapati05@gmail.com>
2022-06-18 21:27:05 +0530

    StartUp Cards

---
+++ src/components/Startup_card/Startup_card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Startup_card.css';
+
+function Startup_card(props) {
+  const {img, Name, text, number}=props;
+
+  return (
+    <>
+        <div className="card-container">
+          <img className="card-image" src={img} alt={Name} />
+          <div className="card-number">{number}</div>
+          <div className="card-text">{text}</div>
+        </div>
+    </>
+  );
+}
+
+export default Startup_card
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
+import StartupCard from "../../components/Startup_card/Startup_card";
+import CardData from '../../data/startup_card_content.json'
+import './Home.css'
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="Homecards">
+        {
+          CardData.map((ArrayElement)=>(
+            <StartupCard 
+              img = {ArrayElement.img}
+              Name = {ArrayElement.name}
+              text = {ArrayElement.text}
+              number = {ArrayElement.number}
+            />
+          ))
+
+        }
+      </div>
+    </>
+  );
+  
 }
 
 export default Home;
