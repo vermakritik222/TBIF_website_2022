78020f3aa1630f5990f083cad9d8c16f6b466712
yamuprajapati05@gmail.com <yamuprajapati05@gmail.com>
2022-06-29 22:09:07 +0530

    headline

---
+++ src/components/HeadLine/HeadLine.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HeadLine.css';
+
+function HeadLine() {
+  return (
+    <>
+        <div className="headline-container"> 
+            <div className="headline-text"> 
+              <p id = "movingText">
+                  News:  &nbsp; <a href="#">Startups and Professionals are invited for availing working space at Research and Innovation Park, IIT Ropar - Click here for details.</a> 
+              </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default HeadLine
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import StartupCard from "../../components/Startup_card/Startup_card";
 import CardData from '../../data/startup_card_content.json'
+import HeadLine from "../../components/HeadLine/HeadLine";
 import './Home.css'
 
 function Home() {
   return (
     <>
+      <div className="HeadLine">
+        <HeadLine/>
+      </div>  
+
       <div className="Homecards">
         {
           CardData.map((ArrayElement)=>(
