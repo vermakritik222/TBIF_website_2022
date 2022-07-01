a0488f550fb72fe8effa9db88060231af3bd756e
yamuprajapati05@gmail.com <yamuprajapati05@gmail.com>
2022-07-01 23:36:27 +0530

    career page

---
+++ src/pages/Career/Career.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CareerPage from '../../components/CareerPage/CareerPage'
+
+function Career() {
+  return (
+    <>
+    <CareerPage/>
+    </>
+  )
+}
+
+export default Career
