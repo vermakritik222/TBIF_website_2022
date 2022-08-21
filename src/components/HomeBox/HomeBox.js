bc05f3e7f4517e5be3565c7d51bafd3c2edbeb30
kritik <kritikverma2002@gmail.com>
2022-08-21 23:00:38 +0530

    final1

--- src/components/HomeBox/HomeBox.js
+++ src/components/HomeBox/HomeBox.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./HomeBox.scss";
 
 function HomeBox() {
-  return <div className="container homeBox__main">HomeBox</div>;
+  return (
+    <div className="homeBox">
+      <div className="container homeBox__main">
+        <h2>TECHNOLOGY BUSINESS INCUBATOR FOUNDATION IIT ROPAR</h2>
+        <p>
+          IIT Ropar's Technology Business Incubator Foundation is engaged in
+          pushing forward with the mission of promoting entrepreneurial
+          activities among our students and faculties and people of the region.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default HomeBox;
