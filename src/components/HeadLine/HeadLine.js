809a07dd83d31113f4430c0b6c75847a502107ee
kritik <kritikverma2002@gmail.com>
2023-01-03 03:35:11 +0530

    head line

--- src/components/HeadLine/HeadLine.js
+++ src/components/HeadLine/HeadLine.js
@@ -7,7 +7,7 @@ function HeadLine() {
         <div className="headline-container"> 
             <div className="headline-text"> 
               <p id = "movingText">
-                  News:  &nbsp; <a href="/career">We are looking for a Chief Executive Officer (CEO), IIT Ropar - Click here for details.</a> 
+                  News:  &nbsp; <a href="/">Click here for details.</a> 
               </p>
             </div>
         </div>
